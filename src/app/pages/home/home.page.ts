0dcea98f0418213f37f2a0648bb3e97edde49a56
malcom <malcompozo@gmail.com>
2021-10-02 12:07:08 -0300

    practica de componentes Ionic, introduccion básica, navegacion entre páginas, implementacion de backbutton, modulacion de componentes 'Header', implementacion de ion-list

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirecTo: '/alert'
+    }
+  ];
+
+  constructor() {}
+
+}
